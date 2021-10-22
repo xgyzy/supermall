7d6a65ea954832379a3ae8732507082354627577
xgyzy <646915778@qq.com>
2021-10-22 17:02:57 +0800

    https://github.com/xgyzy/supermall.git

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import router from './router'
+Vue.confingVue.config.productionTip =false;
+new Vue({
+    render: h => h(App),
+    router
+}).$mount('#app')
