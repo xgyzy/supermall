2a7336927d4b38c5ce02107cb3672b3b91a8e57a
xgyzy <646915778@qq.com>
2021-10-29 16:35:45 +0800

    https://github.com/xgyzy/supermall.git

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,20 +4,25 @@
         <div slot="center">购物街</div>
     </nav-bar>
     <home-swiper :banners="banners"></home-swiper>
-
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
 </div>
 </template>
 <script>
 import NavBar from 'components/common/navbar/NavBar'
 import HomeSwiper from './childComps/HomeSwiper'
 import {getHomeMultidata} from 'network/home'
+import RecommendView from './childComps/RecommendView.vue'
+import FeatureView from './childComps/FeatureView.vue'
 
 export default {
     name:"Home",
     components:{
         NavBar,
         getHomeMultidata,
-        HomeSwiper
+        HomeSwiper,
+        RecommendView,
+        FeatureView
     },
     data() {
         return {
@@ -29,14 +34,23 @@ export default {
         getHomeMultidata().then(res=>{
             // this.result=res
             this.banners =res.data.banner.list
-            this.recommends=res.data.recommend
+            this.recommends=res.data.recommend.list
+            console.log(this.recommends)
         })
     }
 }
 </script>
 <style scoped>
+#home{
+    padding-top: 44px;
+}
 .home-nav{
     background-color: var(--color-tint);
+    position: fixed;
     color: #fff;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
 }
 </style>
